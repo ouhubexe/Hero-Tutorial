03e6656c65dac527b6a065a6faa472c71d73e112
ouhubexe <ou_hub_exe@hotmail.com>
2018-11-08 16:34:12 +0700

    Ready to Deploy

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,10 +5,10 @@ import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heros',
-  templateUrl: './heros.component.html',
-  styleUrls: ['./heros.component.css']
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
-export class HerosComponent implements OnInit {
+export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
   selectedHero: Hero;
@@ -21,8 +21,6 @@ export class HerosComponent implements OnInit {
 
   ngOnInit() {
       this.getHeroes();
-      console.log(this.heroes);
-
   }
 
   onSelect(hero: Hero): void {
@@ -32,5 +30,21 @@ export class HerosComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
+
+  delete(hero:Hero):void{
+    this.heroes = this.heroes.filter(h=>h!==hero);
+    this.heroService.deleteHero(hero).subscribe();
+
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
 
 }
